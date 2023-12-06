b453c422b7f62c38aa2b1be749b2cbc5d058fc85
Harsh Deep <hdeep61034@gmail.com>
2023-12-06 16:27:26 +0530

    main section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <>
       <Navbar />
-      <h1>React App</h1>
+      <div className="main">
+        <div className="main-logo stroke">FliprAi</div>
+        <div className="main-title">
+          <p>
+            Enjoy Your Guilty Pleasure!! Bypassing All the AI DETECTIVES 🤫{" "}
+          </p>
+        </div>
+        <div className="main-btns">
+          <button className="btn-pr">Humanize_iT</button>
+          <button className="btn-sr">Flip</button>
+        </div>
+      </div>
     </>
   );
 }
