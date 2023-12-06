f89a06a096a90789165d453dbc0b86fb899af720
Harsh Deep <hdeep61034@gmail.com>
2023-12-06 15:16:00 +0530

    made Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,16 @@
+import "./Navbar.css";
+
 const Navbar = () => {
-  return <>Nav</>;
+  return (
+    <>
+      <nav>
+        <div className="nav-logo">FliprAi</div>
+        <div className="nav-links-div">
+          <div className="nav-link btn-txt">Home</div>
+          <div className="nav-link btn-pr">Flip</div>
+        </div>
+      </nav>
+    </>
+  );
 };
 export default Navbar;
